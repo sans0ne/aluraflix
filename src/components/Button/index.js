e6cb9afd62bbdb162567c13c61fe84bc95f9e3c3
sans0ne <aniello_sansone@hotmail.com>
2020-08-02 10:33:30 -0300

    Commit final aula 4

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,11 @@
-import styled from 'styled-components'
+/* eslint-disable linebreak-style */
+import styled from 'styled-components';
 
 const Button = styled.button`
 
 color: var(--white);
 border: 1px solid var(--white);
+background: var(--background);
 box-sizing: border-box;
 cursor: pointer;
 padding: 16px 24px;
@@ -20,5 +22,5 @@ transition: opacity .3s;
 &:focus {
     opacity: .5;
 }
-`
-export default Button
+`;
+export default Button;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,31 +1,98 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-            
-            <form>
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#000',
+  };
 
-                <label>
-                    Nome da Categoria:
-                <input
-                    type="text"
-                />
-                </label>
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-                <button>
-                    Cadastrar
-                </button>
-            </form>
-            
-            <Link to='/'>
-                Ir para Home
-          </Link>
-        </PageDefault>
-    )
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  }, []);
+
+  function setValue(target) {
+    const { name, value } = target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  }
+  const handleChange = (e) => {
+    setValue(e.target);
+  };
+
+  const { nome, descricao, cor } = values;
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {nome}
+      </h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+        setValues(valoresIniciais);
+      }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={cor}
+          onChange={handleChange}
+        />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={categoria.nome}>{categoria.nome}</li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para Home
+      </Link>
+    </PageDefault>
+  );
 }
 
-export default CadastroCategoria
+export default CadastroCategoria;
